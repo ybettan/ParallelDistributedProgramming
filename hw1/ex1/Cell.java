627c035614361340e2b4848334df6bff5566cf70
Yoni Bettan <ybettan@redhat.com>
2017-11-14 21:11:03 +0200

    ex1/Cell.java : updated Cell to be 1D instead of 2D
            * deleted the oldVersion Cell pointer (only 1D now)
            * no more access to any class variable (only methods are public)
            * added a copy constructor
            * added isAlive(), wasUpdated() and getGen() methods
    
    test/CellTest.java : wrote a unit test for Cell class
    
            * test PASS
    
    Signed-off-by: Yoni Bettan <ybettan@redhat.com>

--- hw1/ex1/Cell.java
+++ hw1/ex1/Cell.java
@@ -5,68 +5,77 @@ package ex1;
 
 public class Cell {
 
-    public boolean isAlive;
-
+    private boolean isAlive;
     private int gen;
     private int checkedNeighbors;
     private int aliveNeighbors;
+    private boolean wasUpdated;
     // not all cells have 8 neighbors
-    private int maxNeighbors;        
-    // a copy of the old version for reading 
-    private Cell oldVersion;
+    private final int MAX_NEIGHBORS;        
+
 
     public Cell(boolean isAlive, int maxNeighbors) {
         this.isAlive = isAlive;
         gen = 0;
         checkedNeighbors = 0;
         aliveNeighbors = 0;
-        this.maxNeighbors = maxNeighbors;
-        // oldVersion is initiallize no null by default
+        wasUpdated = false;
+        this.MAX_NEIGHBORS = maxNeighbors;
+    }
+
+    public Cell(Cell ref) {
+        this.isAlive = ref.isAlive;
+        this.gen = ref.gen;
+        this.checkedNeighbors = ref.checkedNeighbors;
+        this.aliveNeighbors = ref.aliveNeighbors;
+        this.wasUpdated = ref.wasUpdated;
+        this.MAX_NEIGHBORS = ref.MAX_NEIGHBORS;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public boolean wasUpdated() {
+        boolean tmp = wasUpdated;
+        wasUpdated = false;
+        return tmp;
+    }
+
+    public int getGen() {
+        return gen;
     }
 
     // given a neighbor cell we can add it to the calculation for our cell
     public void addNeighbor(Cell c) {
-
         //FIXME: for debugging - remove when done
         if (gen != c.gen)
             System.err.println("asserer faild at line " + 
                     new Throwable().getStackTrace()[0].getLineNumber());
-
         checkedNeighbors++;
-        if (c.isAlive) {
+        if (c.isAlive()) {
             aliveNeighbors++;
         }
-
         // if we handlend all neighbors we can update the cell to next gen
-        if (checkedNeighbors == maxNeighbors) {
+        if (checkedNeighbors == MAX_NEIGHBORS) {
             moveToNextGen();
         }
     }
     
     private void moveToNextGen() {
-
         // FIXME: just for debugging - remove when finish
         if (aliveNeighbors > checkedNeighbors) 
             System.err.println("asserer faild at line " + 
                     new Throwable().getStackTrace()[0].getLineNumber());
-
-        // update oldVersion to current version
-        // NOTE: we update only relevant data for reading
-        if (oldVersion == null) {
-            // oldVersion not yet allocated
-            oldVersion = new Cell(isAlive, maxNeighbors);
-            // oldVersion.oldVersion recieve null by default so no loop
-        }
-        oldVersion.gen = gen;
-
         // update current version
-        gen++;
         if (aliveNeighbors == 3 || (aliveNeighbors == 2 && isAlive) ) 
             isAlive = true;
         else 
             isAlive = false;
-        
+        gen++;
         checkedNeighbors = 0;
         aliveNeighbors = 0;
+        wasUpdated = true;
     }
+
 }
--- hw1/tests/CellTest.java
+++ hw1/tests/CellTest.java
@@ -1,165 +1,141 @@
 package tests;
 
+import ex1.Cell;
 
-class Cell {
 
-    boolean isAlive;
-    private int gen;
-    private int checkedNeighbors;
-    private int aliveNeighbors;
-    // not all cells have 8 neighbors
-    private int maxNeighbors;        
-    // a copy of the old version for reading 
-    private Cell oldVersion;
+public class CellTest {
 
-    Cell(boolean isAlive, int maxNeighbors) {
-        this.isAlive = isAlive;
-        gen = 0;
-        checkedNeighbors = 0;
-        aliveNeighbors = 0;
-        this.maxNeighbors = maxNeighbors;
-        // oldVersion is initiallize no null by default
-    }
+    public static void main(String args[]) {
 
-    // given a neighbor cell we can add it to the calculation for our cell
-    void addNeighbor(Cell c) {
+        System.out.print("CellTest...");
 
-        //FIXME: for debugging - remove when done
-        if (gen != c.gen)
-            System.err.println("asserer faild at line " + 
-                    new Throwable().getStackTrace()[0].getLineNumber());
+        /* constructor test & isAlive() test */
+        Cell c1 = new Cell(true, 4);
+        Cell c2 = new Cell(false, 200);
+        Cell c3 = new Cell(true, 15);
+        Cell c4 = new Cell(false, 8);
+        Cell c5 = new Cell(true, 4);
+        Cell c6 = new Cell(false, 200);
+        Cell c7 = new Cell(true, 15);
+        Cell c8 = new Cell(false, 8);
+        test(c1.isAlive() == true);
+        test(c2.isAlive() == false);
+        test(c3.isAlive() == true);
+        test(c4.isAlive() == false);
+        test(c5.isAlive() == true);
+        test(c6.isAlive() == false);
+        test(c7.isAlive() == true);
+        test(c8.isAlive() == false);
 
-        checkedNeighbors++;
-        if (c.isAlive) {
-            aliveNeighbors++;
-        }
+        /* wasUpdated(), getGen() & addNeighbor() test */
+        Cell deadCell = new Cell(false, 4);
+        Cell liveCell = new Cell(true, 4);
+        Cell c9 = new Cell(false, 4);
+        test(c9.wasUpdated() == false);
+        test(c9.getGen() == 0);
+        c9.addNeighbor(deadCell);
+        c9.addNeighbor(deadCell);
+        c9.addNeighbor(deadCell);
+        c9.addNeighbor(deadCell);
+        test(c9.isAlive() == false);
+        test(c9.wasUpdated() == true);
+        test(c9.wasUpdated() == false);
+        test(c9.getGen() == 1);
+        /* this and the next copies are to update the inputCell gen */
+        liveCell.addNeighbor(liveCell);
+        liveCell.addNeighbor(liveCell);
+        liveCell.addNeighbor(liveCell);
+        liveCell.addNeighbor(deadCell);
+        deadCell = new Cell(c9);
+        test(liveCell.isAlive() == true);
+        test(deadCell.isAlive() == false);
+        test(liveCell.getGen() == 1);
+        test(deadCell.getGen() == 1);
+        c9.addNeighbor(liveCell);
+        c9.addNeighbor(deadCell);
+        c9.addNeighbor(deadCell);
+        c9.addNeighbor(deadCell);
+        test(c9.isAlive() == false);
+        test(c9.wasUpdated() == true);
+        test(c9.wasUpdated() == false);
+        test(c9.getGen() == 2);
+        liveCell.addNeighbor(liveCell);
+        liveCell.addNeighbor(liveCell);
+        liveCell.addNeighbor(liveCell);
+        liveCell.addNeighbor(deadCell);
+        deadCell = new Cell(c9);
+        test(liveCell.isAlive() == true);
+        test(deadCell.isAlive() == false);
+        test(liveCell.getGen() == 2);
+        test(deadCell.getGen() == 2);
+        c9.addNeighbor(liveCell);
+        c9.addNeighbor(liveCell);
+        c9.addNeighbor(deadCell);
+        c9.addNeighbor(deadCell);
+        test(c9.isAlive() == false);
+        test(c9.wasUpdated() == true);
+        test(c9.wasUpdated() == false);
+        test(c9.getGen() == 3);
+        liveCell.addNeighbor(liveCell);
+        liveCell.addNeighbor(liveCell);
+        liveCell.addNeighbor(liveCell);
+        liveCell.addNeighbor(deadCell);
+        deadCell = new Cell(c9);
+        test(liveCell.isAlive() == true);
+        test(deadCell.isAlive() == false);
+        test(liveCell.getGen() == 3);
+        test(deadCell.getGen() == 3);
+        c9.addNeighbor(liveCell);
+        c9.addNeighbor(liveCell);
+        c9.addNeighbor(liveCell);
+        c9.addNeighbor(deadCell);
+        test(c9.isAlive() == true);
+        test(c9.wasUpdated() == true);
+        test(c9.wasUpdated() == false);
+        test(c9.getGen() == 4);
+        deadCell.addNeighbor(deadCell);
+        deadCell.addNeighbor(deadCell);
+        deadCell.addNeighbor(deadCell);
+        deadCell.addNeighbor(deadCell);
+        liveCell = new Cell(c9);
+        test(liveCell.isAlive() == true);
+        test(deadCell.isAlive() == false);
+        test(liveCell.getGen() == 4);
+        test(deadCell.getGen() == 4);
+        c9.addNeighbor(liveCell);
+        c9.addNeighbor(liveCell);
+        c9.addNeighbor(deadCell);
+        c9.addNeighbor(deadCell);
+        test(c9.isAlive() == true);
+        test(c9.wasUpdated() == true);
+        test(c9.wasUpdated() == false);
+        test(c9.getGen() == 5);
+        deadCell.addNeighbor(deadCell);
+        deadCell.addNeighbor(deadCell);
+        deadCell.addNeighbor(deadCell);
+        deadCell.addNeighbor(deadCell);
+        liveCell = new Cell(c9);
+        test(liveCell.isAlive() == true);
+        test(deadCell.isAlive() == false);
+        test(liveCell.getGen() == 5);
+        test(deadCell.getGen() == 5);
+        c9.addNeighbor(liveCell);
+        c9.addNeighbor(liveCell);
+        c9.addNeighbor(liveCell);
+        c9.addNeighbor(liveCell);
+        test(c9.isAlive() == false);
+        test(c9.wasUpdated() == true);
+        test(c9.wasUpdated() == false);
+        test(c9.getGen() == 6);
 
-        // if we handlend all neighbors we can update the cell to next gen
-        if (checkedNeighbors == maxNeighbors) {
-            moveToNextGen();
-        }
-    }
-    
-    private void moveToNextGen() {
+        System.out.println("[OK]");
+    } 
 
-        // FIXME: just for debugging - remove when finish
-        if (aliveNeighbors > checkedNeighbors) 
-            System.err.println("asserer faild at line " + 
-                    new Throwable().getStackTrace()[0].getLineNumber());
-
-        // update oldVersion to current version
-        // NOTE: we update only relevant data for reading
-        if (oldVersion == null) {
-            // oldVersion not yet allocated
-            oldVersion = new Cell(isAlive, maxNeighbors);
-            // oldVersion.oldVersion recieve null by default so no loop
-        }
-        oldVersion.gen = gen;
-
-        // update current version
-        gen++;
-        if (aliveNeighbors == 3 || (aliveNeighbors == 2 && isAlive) ) 
-            isAlive = true;
-        else 
-            isAlive = false;
-        
-        checkedNeighbors = 0;
-        aliveNeighbors = 0;
-    }
-
-    //FIXME: remove all -----------------------------------------------------
     private static void test(boolean statment) {
         if (statment == false) 
            System.err.println("ERROR");
     }
 
-    static void callMain() {
-
-        System.out.print("CellTest()...");
-
-       // // test that my debugging assert works
-       // Cell badCell = new Cell(true, 4);
-       // badCell.gen = 5;
-       // Cell c0 = new Cell(false, 4);
-       // c0.addNeighbor(badCell);
-        
-        Cell c1 = new Cell(false, 4);
-        test(c1.gen == 0); 
-        test(c1.checkedNeighbors == 0); 
-        test(c1.aliveNeighbors == 0); 
-        test(c1.maxNeighbors == 4); 
-        test(c1.oldVersion == null); 
-
-        Cell deadCell = new Cell(false, 8);
-        Cell liveCell = new Cell(true, 8);
-
-        c1.addNeighbor(liveCell);
-        test(c1.gen == 0); 
-        test(c1.checkedNeighbors == 1); 
-        test(c1.aliveNeighbors == 1); 
-        test(c1.oldVersion == null); 
-
-        c1.addNeighbor(deadCell);
-        test(c1.gen == 0); 
-        test(c1.checkedNeighbors == 2); 
-        test(c1.aliveNeighbors == 1); 
-        test(c1.oldVersion == null); 
-
-        c1.addNeighbor(liveCell);
-        test(c1.gen == 0); 
-        test(c1.checkedNeighbors == 3); 
-        test(c1.aliveNeighbors == 2); 
-        test(c1.oldVersion == null); 
-
-        c1.addNeighbor(liveCell);
-        test(c1.isAlive == true); 
-        test(c1.gen == 1); 
-        test(c1.checkedNeighbors == 0); 
-        test(c1.aliveNeighbors == 0); 
-        test(c1.oldVersion != null); 
-
-        liveCell.gen++;
-        deadCell.gen++;
-
-        c1.addNeighbor(liveCell);
-        test(c1.gen == 1); 
-        test(c1.checkedNeighbors == 1); 
-        test(c1.aliveNeighbors == 1); 
-        test(c1.oldVersion != null); 
-
-        c1.addNeighbor(liveCell);
-        test(c1.gen == 1); 
-        test(c1.checkedNeighbors == 2); 
-        test(c1.aliveNeighbors == 2); 
-        test(c1.oldVersion != null); 
-
-        c1.addNeighbor(deadCell);
-        test(c1.gen == 1); 
-        test(c1.checkedNeighbors == 3); 
-        test(c1.aliveNeighbors == 2); 
-        test(c1.oldVersion != null); 
-
-        c1.addNeighbor(deadCell);
-        test(c1.isAlive == true);
-        test(c1.gen == 2); 
-        test(c1.checkedNeighbors == 0); 
-        test(c1.aliveNeighbors == 0); 
-
-        liveCell.gen++;
-        deadCell.gen++;
-
-        c1.addNeighbor(liveCell);
-        c1.addNeighbor(deadCell);
-        c1.addNeighbor(deadCell);
-        c1.addNeighbor(deadCell);
-        test(c1.isAlive == false);
-        test(c1.gen == 3); 
-        test(c1.checkedNeighbors == 0); 
-        test(c1.aliveNeighbors == 0); 
-
-        System.out.println("[OK]");
-    }
-    //FIXME: ----------------------------------------------------------------
 }
 
 
