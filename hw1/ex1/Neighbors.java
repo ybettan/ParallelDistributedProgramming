7eee32aeda923100c1bb9891e187eb97c49f7303
Yoni Bettan <ybettan@redhat.com>
2017-11-15 22:08:48 +0200

    ex1/Neighbors.java : added a class thats holds all the Fields neighbors
    tests/NeighborsTest.java : unit test SKELETON for Neighbors
    
            * TODO : implement it
    
    Signed-off-by: Yoni Bettan <ybettan@redhat.com>

---
+++ hw1/ex1/Neighbors.java
@@ -0,0 +1,51 @@
+package ex1;
+
+
+public class Neighbors {
+    Field up, upRight, right, downRight, down, downLeft, left, upLeft;
+
+    Neighbors(Field up, Field upRight, Field right, Field downRight, Field down, 
+            Field downLeft, Field left, Field upLeft) {
+        this.up = up;
+        this.upRight = upRight;
+        this.right = right;
+        this.downRight = downRight;
+        this.down = down;
+        this.downLeft = downLeft;
+        this.left = left;
+        this.upLeft = upLeft;
+    }
+
+    Field getUp() {
+        return up;
+    }
+
+    Field getUpRight() {
+        return upRight;
+    }
+
+    Field getRight() {
+        return right;
+    }
+
+    Field getDownRight() {
+        return downRight;
+    }
+
+    Field getDown() {
+        return down;
+    }
+
+    Field getDownLeft() {
+        return downLeft;
+    }
+
+    Field getLeft() {
+        return left;
+    }
+
+    Field getUpLeft() {
+        return upLeft;
+    }
+
+}
---
+++ hw1/tests/NeighborsTest.java
@@ -0,0 +1,30 @@
+package tests;
+
+import ex1.Neighbors;
+
+
+public class NeighborsTest {
+
+    public static void main(String args[]) {
+
+        System.out.print("NeighborsTest...");
+
+
+
+        System.out.println("[OK]");
+    } 
+
+    private static void test(boolean statment) {
+        if (statment == false) 
+           System.err.println("ERROR");
+    }
+
+}
+
+
+
+
+
+
+
+
